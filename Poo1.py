1651f08070afabe74496606c064fcf90e0fc0d62
Mfergo29 <101641737+Mfergo29@users.noreply.github.com>
2022-03-22 18:33:26 -0300

    Add files via upload

---
+++ Poo1.py
@@ -0,0 +1,19 @@
+ficcion=[{'Frank Herbert':['Dune', 'El Mesías de Dune', 'Hijos de Dune']},
+ {'Arthur Clarke': ['2001: Una odisea espacial', '2010: Odisea dos','2061: Odisea tres']},
+  {'Frederik Pohl': ['Pórtico','Tras el incierto horizonte','El encuentro']}]
+
+romance=[{'Juliana Gray':['Una dama nunca miente','Un caballerosiempre es discreto','Un duque nunca se rinde']},
+{'Jamie Mcguire':['Maravilloso desastre','Inevitable desastre','Un desastre es para siempre']},
+{'Sylvia Day':['No te escondo nada','Reflejada en ti','Atada a ti']}]
+
+aventura=[{'JR. Tolkien':['El señor de los anillos 1','El señor de los anillos 2','El señor de los anillos 3']},
+{'JK Rowling':['Harry Potter 1','Harry Potter 2','Harry Potter 3']},
+{'Robert Jordan':['La rueda del tiempo 1','La rueda del tiempo 2','La rueda del tiempo 3']}]
+
+
+main=input('Que desea hacer: \n 1. Ver inventario \n 2. Manejar Arriendos \n 3. Categorizar Inventario \n 4. Modificar Catalogo \n')
+
+if main==1:
+    
+
+print(ficcion[1]['Arthur Clarke'][0])
